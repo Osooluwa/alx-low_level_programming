f87b8b82ec1bf83687f33be389e756832a094989
Osooluwa <osooluwaajibola@gmail.com>
2023-12-15 20:48:04 -0800

    a program that prints the lowercase alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,14 +8,8 @@
 
 int main(void)
 {
-	int n;
 	char ch;
-
-	for (n = 48; n < 58; n++)
-	{
-		putchar(n);
-	}
-	for (ch = 'a'; ch <= 'f'; ch++)
+	for (ch = 'z'; ch >= 'a'; ch--)
 	{
 		putchar(ch);
 	}
