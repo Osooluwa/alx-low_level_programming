536a6c610da8a78e88f7b4d0a55c33b07ebf89b4
Osooluwa <osooluwaajibola@gmail.com>
2023-12-19 12:36:28 -0800

    a function that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main -a function that prints the alphabet,
+ * in lowercase, then new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
