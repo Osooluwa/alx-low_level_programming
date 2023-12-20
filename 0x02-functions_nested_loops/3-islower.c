5729ea0dbb37b005b5c563c25567c513157d17f1
Osooluwa <osooluwaajibola@gmail.com>
2023-12-20 10:15:28 -0800

    a function that checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - a function that checks for lowercase character
  * 1 if lowercase otherwise 0
- * c - character to be checked
+ * @c - character to be checked
  * Return: Always 0 (Success)
  */
 
