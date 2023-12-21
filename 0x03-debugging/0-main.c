2098b3a1de1f5a91756f37bac3fb0a7d3bf6e60c
Osooluwa <osooluwaajibola@gmail.com>
2023-12-21 13:25:06 -0800

    prints if a number is negative or positive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - prints if integer is negative or positive
+ * @i: character to check
+ * Return: Always 0
+ */
+
+void positive_or_negative(int i)
+{
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
