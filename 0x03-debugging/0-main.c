214d9e21c7fee282e63d6942198cc1aba2837682
Osooluwa <osooluwaajibola@gmail.com>
2023-12-21 13:29:31 -0800

    rint if integer is negative or positive

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,12 +2,13 @@
 
 /**
  * positive_or_negative - prints if integer is negative or positive
- * @i: character to check
  * Return: Always 0
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
+	int i;
+
 	i = 0;
 	positive_or_negative(i);
 
