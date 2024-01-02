8d140955d002ad80421359a0d46488693f158570
Osooluwa <osooluwaajibola@gmail.com>
2024-01-02 14:24:36 -0800

    a function that prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string
+ * in reverse
+ * @s: the string
+ */
+
+void print_rev(char *s)
+{
+	int a, b;
+
+	a = strlen(s);
+	for (b = a - 1; b >= 0; b--)
+	{
+		_putchar(a[b]);
+	}
+}
