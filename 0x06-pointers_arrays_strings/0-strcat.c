d1023cd6bb7ec76f27745fa8fe555239559d04af
Osooluwa <osooluwaajibola@gmail.com>
2024-01-04 13:34:39 -0800

    a function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,32 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *dest, char *src;
+	char str1[100] = "Osooluwa", str2[100] = "ALXSE";
 
-	*dest = "Osooluwa";
-	*src = " is a woman in ALX SE";
+	char str3[100];
 
-	printf("%s" + "%s\n, *dest, *src);
+	int i = 0, j = 0;
+
+	printf("\nFirst string: %s", str1);
+	printf("\nSecond string: %s", str2);
+
+	while (str1[i] != '\0')
+	{
+		str3[j] = str1[i];
+		i++;
+		j++;
+	}
+	while (str2[i] != '\0')
+	{
+		str3[j] = str2[i];
+		i++;
+		j++;
+	}
+	str3[j] = '\0';
+
+	printf("\nConcatenated string: %s", str3);
+
 	return (0);
 }
+	   
+
