5c08ae1a50fb306444f4a1c74ee4c5ec7f2591c6
Osooluwa <osooluwaajibola@gmail.com>
2024-02-15 06:30:01 -0800

    Tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,20 +19,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return(0);
+		return (0);
 
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
-	if (0 == -1 || r == -1 || w== -1 || w != r)
+	if (0 == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
-		return(0);
+		return (0);
 	}
 
 	free(buffer);
 	close(o);
 
-	return(w);
+	return (w);
 }
