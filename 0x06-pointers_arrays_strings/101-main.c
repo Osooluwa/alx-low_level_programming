6b066a82a689e174b0991bb31eeb708c035f706e
Osooluwa <osooluwaajibola@gmail.com>
2024-02-16 11:41:08 -0800

    Fixed

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int print_number();
+
+	print_number(0);
+	putchar('\n');
+	return (0);
+}
